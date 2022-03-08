d5bec68ebf7cde782f9712827127090225c3118b
ehsan2793 <ehsan2793@gmail.com>
2022-03-07 23:36:34 -0800

    data get saved

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from tkinter import *
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    with open("data.txt", "a") as data_file:
+        data_file.write(f' {website} | {email} | {password}\n')
+    website_entry.delete(0,END)
+    password_entry.delete(0, END)
+
+
+# -------------------------------------
 
 window = Tk()
 window.title("Password Manager")
@@ -23,8 +34,10 @@ password_label.grid(row=3, column=0)
 # Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, 'rahimiehsan1997@gmail.com')
 password_entry = Entry(width=22)
 password_entry.grid(row=3, column=1)
 
@@ -32,7 +45,7 @@ password_entry.grid(row=3, column=1)
 generate_password_btn = Button(text="Generate Password", width=14)
 generate_password_btn.grid(row=3, column=2)
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 # ---------------------------- #
